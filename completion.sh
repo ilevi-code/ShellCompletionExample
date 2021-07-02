ae8158af3d8617ec52c4103a34059cadbccaf0ef
ilevy <itayill01@gmail.com>
2021-07-03 01:10:05 +0300

    Fix completion script reply

--- completion.sh
+++ completion.sh
@@ -1,7 +1,7 @@
-_foo_completion() {
+function _foo_completion() {
     local IFS=$'
 '
-    COMPREPLY=$(COMP_CWORD=$COMP_CWORD _FOO_COMPLETE=complete $1 ${COMP_WORDS[@]})
+    COMPREPLY=( $(COMP_CWORD=$COMP_CWORD _FOO_COMPLETE=complete $1 ${COMP_WORDS[@]:1}) )
     return 0
 }
 
