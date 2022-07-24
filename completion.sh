3683bc74bee3e385857e9c59869e8fe2b4ec187b
ilevy <itayill01@gmail.com>
2022-07-24 20:56:48 +0300

    Ignore colons in argument parsing

--- completion.sh
+++ completion.sh
@@ -1,8 +1,7 @@
 function _foo_completion() {
-    local IFS=$'
-'
-    COMPREPLY=( $(COMP_CWORD=$COMP_CWORD _FOO_COMPLETE=complete $1 ${COMP_WORDS[@]:1}) )
-    return 0
+	_get_comp_words_by_ref -n ':'
+	COMPREPLY=( $(COMP_CWORD=$cword _FOO_COMPLETE=complete $1 ${words[@]:1}) )
+	return 0
 }
 
 complete -F _foo_completion foo
